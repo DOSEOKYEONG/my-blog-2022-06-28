65f53363a19c753079b376eed58e9f017f5978d9
DOSEOKYEONG <dho88dho@naver.com>
2022-06-28 17:32:23 +0900

    32강까지 완료

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "asasdasdad",
+    description: "도석영의 블로그1234",
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+  ],
+};
